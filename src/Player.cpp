6530bb19689474ceb064a18dcc8ce9abe54cbe64
SkyPh03nix <monja.biendl@gmail.com>
2025-08-26 10:59:02 +0200

    recentered player positon and added comments

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,12 +12,16 @@ Player::Player(sf::Texture& walkTexture,sf::Texture& idleTexture, sf::Vector2f p
     initAnimationSet(idleTexture, "idle", 6, 0.15f, 4);
 
     // set initial texture and sprite
-    sprite.setPosition(pos);
-    sprite.setScale(4.f,4.f);
-    
     currentAnimId = anims.play("idle_down");
     anims.applyToSprite(currentAnimId, sprite);
     currentAnimName = "idle_down";
+    
+    // center origin
+    sf::FloatRect bounds = sprite.getGlobalBounds();
+    sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f); //origin to center
+
+    sprite.setPosition(pos);
+    sprite.setScale(4.f,4.f);
 }
 
 void Player::initAnimationSet(sf::Texture& texture, const std::string& prefix, int frameCount, float frameTime, int directionsCount) {
@@ -31,6 +35,7 @@ void Player::initAnimationSet(sf::Texture& texture, const std::string& prefix, i
             frames.emplace_back(i * frameWidth, dir * frameHeight, frameWidth, frameHeight);
         }
         std::string animName = prefix + "_";
+        // auto naming convention: walk_down, walk_up, walk_left, walk_right
         switch (dir) {
             case 0: animName += "down"; break;
             case 1: animName += "up"; break;
@@ -43,7 +48,6 @@ void Player::initAnimationSet(sf::Texture& texture, const std::string& prefix, i
 }
 
 void Player::update(float deltaTime, const sf::RenderWindow& window) {
-    sf::Vector2u windowSize = window.getSize();
 
     sf::Vector2f movement(0.f, 0.f);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) movement.y -= speed * deltaTime;
@@ -79,30 +83,41 @@ void Player::update(float deltaTime, const sf::RenderWindow& window) {
         }
     }
 
+    //update animation
     if (nextAnim != currentAnimName) {
         currentAnimId = anims.play(nextAnim);
         currentAnimName = nextAnim;
     }
 
-    //update animation
     anims.update(deltaTime);
     if (currentAnimId != -1) {
         anims.applyToSprite(currentAnimId, sprite);
     }
 
-    //Check for window border
-    sf::FloatRect bounds = getGlobalBounds();
-    sf::Vector2f pos = sprite.getPosition();
+    //Checking for window bounds and correct position if outside (currently disabled because of camera movement)
+        //sf::FloatRect bounds = getGlobalBounds();
+        //sf::Vector2f pos = sprite.getPosition();
 
-    //if (bounds.left < 0) pos.x -= bounds.left; // Left
-    //if (bounds.top < 0) pos.y -= bounds.top;   // Top
-    //if (bounds.left + bounds.width > windowSize.x) pos.x -= (bounds.left + bounds.width - windowSize.x); // Right
-    //if (bounds.top + bounds.height > windowSize.y) pos.y -= (bounds.top + bounds.height - windowSize.y); // Bottom
-    sprite.setPosition(pos);
+        //if (bounds.left < 0) pos.x -= bounds.left; // Left
+        //if (bounds.top < 0) pos.y -= bounds.top;   // Top
+        //if (bounds.left + bounds.width > windowSize.x) pos.x -= (bounds.left + bounds.width - windowSize.x); // Right
+        //if (bounds.top + bounds.height > windowSize.y) pos.y -= (bounds.top + bounds.height - windowSize.y); // Bottom
+        //sprite.setPosition(pos);
 }
 
 void Player::draw(sf::RenderWindow& window) {
     window.draw(sprite);
+
+    //draw bounds of player
+    sf::FloatRect bounds = getGlobalBounds();
+
+    sf::RectangleShape rect;
+    rect.setPosition(bounds.left, bounds.top);
+    rect.setSize(sf::Vector2f(bounds.width, bounds.height));
+    rect.setFillColor(sf::Color::Transparent);
+    rect.setOutlineColor(sf::Color::Red);
+    rect.setOutlineThickness(1.f);
+    window.draw(rect);    
 }
 
 void Player::setColor(const sf::Color& col) {
@@ -110,6 +125,7 @@ void Player::setColor(const sf::Color& col) {
 }
 
 sf::FloatRect Player::getGlobalBounds() const {
+    //custom function so the collision box is smaller and centered
     float boxSize = 32.f;
     float halfBox = boxSize / 2.f;
 
--- src/game.cpp
+++ src/game.cpp
@@ -8,41 +8,37 @@
 #include "RecourceManager.h"
 
 void Game::initObjects() {
-    //Karl
+    //Player
     auto player = std::make_unique<Player>(
         resources.getTexture("player_walk"),
         resources.getTexture("player_idle"),
         sf::Vector2f(window.getSize().x/2.f-32.f, window.getSize().y/2.f-32.f),
         300.f
     );
-    //Button
-    sf::Vector2f buttonSize(100.f,50.f);
-    sf::Vector2f buttonPos(100, 100);
 
-    auto button = std::make_unique<Button>(
-        buttonSize,
-        buttonPos,
-        "Make Red" //TODO fitTextToButton() function
-    );
-    button->setOnClick([&p = *player](){
-        p.setColor(sf::Color::Red);
-    });
+    //Buttons
+    sf::Vector2f buttonSize(100.f,50.f);
+
+    //Button 1
+    sf::Vector2f buttonPos(100, 100);
+    auto button = std::make_unique<Button>(buttonSize,buttonPos,"Make Red" ); //TODO fitTextToButton() function
+    button->setOnClick([&p = *player](){p.setColor(sf::Color::Red);});
     
     //Button 2
     sf::Vector2f button2Pos(100, 200);
-    auto button2 = std::make_unique<Button>(
-        buttonSize,
-        button2Pos,
-        "Make Green"
-    );
-    button2->setOnClick([&p = *player](){
-        p.setColor(sf::Color::Green);
-    });
+    auto button2 = std::make_unique<Button>(buttonSize,button2Pos,"Make Green");
+    button2->setOnClick([&p = *player](){p.setColor(sf::Color::Green);});
+
+    //Button 3
+    sf::Vector2f button3Pos(100, 300);
+    auto button3 = std::make_unique<Button>(buttonSize,button3Pos,"Reset Color");
+    button3->setOnClick([&p = *player](){p.setColor(sf::Color::White);});
     
     // !!! only do this at end of function (move destroys the local objects) !!!
     gameObjects.push_back(std::move(player));
     gameObjects.push_back(std::move(button)); 
-    gameObjects.push_back(std::move(button2));   
+    gameObjects.push_back(std::move(button2));
+    gameObjects.push_back(std::move(button3)); 
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
@@ -50,10 +46,9 @@ void Game::initTextures() {
     resources.loadTexture("player_walk", "assets/used/Slime1_Walk_full.png");
     resources.loadTexture("player_idle", "assets/used/Slime1_Idle_full.png");
     resources.loadTexture("portal", "assets/used/Portals3.png");
-    //resources.loadTexture("background", "assets/used/background2.png");
     //resources.loadTexture("background", "assets/PixelArtPack/SceneOverview.png"); // for now better
     resources.loadTexture("background", "assets/grassbg.png");
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << __PRETTY_FUNCTION__ << std::endl; //debug
 }
 
 Game::Game() : portals() {
@@ -65,7 +60,7 @@ Game::Game() : portals() {
     camera.setSize(window.getSize().x, window.getSize().y);
 
     portals.setTexture(resources.getTexture("portal"));
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << __PRETTY_FUNCTION__ << std::endl; //debug
 }
 
 void Game::run() {
@@ -81,32 +76,28 @@ void Game::initWindow() {
     sf::VideoMode mode = sf::VideoMode::getDesktopMode();
     window.create(mode, "Karl", sf::Style::Fullscreen);
     window.setFramerateLimit(60);
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << __PRETTY_FUNCTION__ << std::endl; // debug
 }
 
 void Game::initBackground() {
     backgroundSprite.setTexture(resources.getTexture("background"));
-
-    sf::Vector2u windowSize = window.getSize();
-    sf::Vector2u textureSize = resources.getTexture("background").getSize();
-
-    //float scaleX = static_cast<float>(windowSize.x) / textureSize.x;
-    //float scaleY = static_cast<float>(windowSize.y) / textureSize.y;
-
     backgroundSprite.setScale(4, 4);
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << __PRETTY_FUNCTION__ << std::endl; //debug
 }
 
 void Game::processEvents() {
     sf::Event event;
     while (window.pollEvent(event)) {
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-            window.close();
-        }
-        if(event.type == sf::Event::MouseButtonPressed) {
+        if (event.type == sf::Event::KeyPressed) {
+            if (event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        } else if(event.type == sf::Event::MouseButtonPressed) {
             sf::Vector2f mouseWorld = window.mapPixelToCoords(sf::Mouse::getPosition(window));
             portals.handleInput(mouseWorld, event.mouseButton.button);
-        }
+        } else if(event.type == sf::Event::Closed) { //if we want to close the window by clicking the X button
+            window.close(); 
+        } 
     }
 }
 
@@ -136,23 +127,23 @@ void Game::render() {
     sf::Vector2u texSize = resources.getTexture("background").getSize();
     sf::Vector2u winSize = window.getSize();
 
-    // Startposition auf die nächste untere linke Tile runden
+    // round to nearest tile size below
     float startX = std::floor((camPos.x - winSize.x / 2.f) / texSize.x) * texSize.x;
     float startY = std::floor((camPos.y - winSize.y / 2.f) / texSize.y) * texSize.y;
 
-    // Anzahl der Tiles: immer +2 extra für den rechten und unteren Rand
+    // number of tiles needed to fill the view
     int tilesX = winSize.x / texSize.x + 2;
     int tilesY = winSize.y / texSize.y + 2;
 
     sf::Sprite bg(resources.getTexture("background"));
 
+    //draw all background tiles
     for (int i = 0; i < tilesX; ++i) {
         for (int j = 0; j < tilesY; ++j) {
             bg.setPosition(startX + i * texSize.x, startY + j * texSize.y);
-            window.draw(bg);
+            window.draw(bg); 
         }
     }
-    //window.draw(backgroundSprite);
 
     portals.draw(window);
 
