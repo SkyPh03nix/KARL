d336fb7fd4f325b079ca5ccad9fda0588e5d8b1d
SkyPh03nix <monja.biendl@gmail.com>
2025-09-09 13:36:50 +0200

    added class for dropped items

--- include/Item.h
+++ include/Item.h
@@ -2,7 +2,9 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <cmath>
 #include <functional>
+#include "GameObject.h"
 
 enum class Type {
     WEAPON,
@@ -37,4 +39,49 @@ private:
     sf::Texture* texture;
     int quantity;
     std::function<void()> useEffect; // Function to define the effect when the item is used
+};
+
+class DroppedItem :public GameObject {
+private:
+    Item item;
+    sf::Sprite sprite;
+    bool pickedUp = false;
+    float pickingRadius = 30.f;
+
+    public:
+    DroppedItem(const Item& item, const sf::Vector2f& position)
+        : item(item) {
+        if (item.getTexture()) {
+            sprite.setTexture(*item.getTexture());
+            //sprite.setScale(1.f, 1.f);
+            sprite.setOrigin(sprite.getLocalBounds().width/2.f, sprite.getLocalBounds().height/2.f);
+        }
+            
+        sprite.setPosition(position);
+    }
+
+    void update(float deltaTime, const sf::RenderWindow& window) override {
+        if (pickedUp) return;
+
+        sprite.setPosition(sprite.getPosition().x , sprite.getPosition().y + std::sin(deltaTime) * 10.f); //TODO check if correct
+    }
+
+    void draw(sf::RenderWindow& window) override {
+        if (!pickedUp)
+            window.draw(sprite);
+    }
+
+    bool checkPickup(const sf::Vector2f& playerPos) {
+        if (pickedUp) return false;
+
+        float distance = std::hypot(playerPos.x - sprite.getPosition().x, playerPos.y - sprite.getPosition().y);
+        if (distance <= pickingRadius) {
+            pickedUp = true;
+            return true;
+        }
+        return false;
+    }
+    const Item& getItem() const { return item; }
+    bool isPickedUp() const { return pickedUp; }
+    sf::Vector2f getPosition() const { return sprite.getPosition(); }
 };
